96d09ecf0a589ccb66726c04b08e94e2566a5ada
Manuel Spitschan <mspits@sas.upenn.edu>
2015-03-01 22:45:43 -0500

    Added MASTER file for luxotonic project

---
+++ Spitschan_Luxotonic/MASTER.m
@@ -0,0 +1,22 @@
+% Analysis of Luxotonic data
+
+%% Data AB
+% First session (A092014B)
+subject = 'A092014B'; session_dir = '/data/jag/mspitschan/Imaging/Protocols/MRLuxotonic/A092014B'; denoise(session_dir,subject,1);
+
+% Second session (A092714B)
+subject = 'A092014B'; session_dir = '/data/jag/mspitschan/Imaging/Protocols/MRLuxotonic/A092714B'; denoise(session_dir,subject,1);
+
+%% Data GA
+% First session (G092014A)
+subject = 'G092014A'; session_dir = '/data/jag/mspitschan/Imaging/Protocols/MRLuxotonic/G092014A'; denoise(session_dir,subject,1);
+
+% Second session (G092014A)
+subject = 'G092014A'; session_dir = '/data/jag/mspitschan/Imaging/Protocols/MRLuxotonic/G092714A'; denoise(session_dir,subject,1);
+
+%% Data MS
+% First session (M120514S)
+subject = 'M120514S'; session_dir = '/data/jag/mspitschan/Imaging/Protocols/MRLuxotonic/M120514S'; denoise(session_dir,subject,1);
+
+% Second session (M121514S)
+subject = 'M120514S'; session_dir = '/data/jag/mspitschan/Imaging/Protocols/MRLuxotonic/M121514S'; denoise(session_dir,subject,1);
