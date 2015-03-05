5fe739b03765c2c6d9f0ce53da98c635f1da8e44
Manuel Spitschan <mspits@sas.upenn.edu>
2015-03-05 12:03:24 -0500

    Replaced file with newer version
    Signed-off-by: Manuel Spitschan <mspits@sas.upenn.edu>

--- Spitschan_Luxotonic/matlab/func_apply_reg.m
+++ Spitschan_Luxotonic/matlab/func_apply_reg.m
@@ -1,39 +1,25 @@
 function outdir = func_apply_reg(session_dirs,func,out_dir)
 
-%   Following feat_stats, this function will apply registrations so that
-%   higher level feat will run.
-%
-%   inputs:
-%   session_dirs - cell containing strings of the paths to the session
-%   directories of interest
-%   subject_name - freesurfer subject name
-%   dirs - vector of feat directories (e.g. [1 4 7 10 13 16])
-%   directories of interest
-%   func - functional data file (default - 'dbrf.tf')
+%   Following feat_stats, this function will apply registrations on any
+%   func file.
 %
 %   Written by Andrew S Bock Dec 2014
 %
 %   3/4/15  ms      Pulled out of feat_higher_level
+%   3/5/15  ms      For use for dbrf.tf registration
 
 %% Set default parameters
 if ~exist('session_dirs','var')
     error('"session_dirs" not defined')
 end
-if ~exist('subject_name','var')
-    error('"subject_name" not defined')
-end
-if ~exist('dirs','var')
-    error('"dirs" not defined') % feat directories
-end
 if ~exist('func','var')
     func = 'dbrf.tf'; % functional data file
 end
-if ~exist('BACKUP', 'var')
-    BACKUP = false;
-end
-if ~exist('OVERWRITE_REG_STANDARD', 'var')
-    OVERWRITE_REG_STANDARD = false;
-end
+
+feat_dir = 'firstlevel.feat';
+
+mkdir(fullfile(out_dir, 'timeseries'));
+outdir = fullfile(out_dir, 'timeseries');
 
 %% Loop through session directories
 for s = 1:length(session_dirs)
@@ -50,10 +36,19 @@ for s = 1:length(session_dirs)
     % Copy over bbregister registration file
     % overwrite the example_func2standard.mat in each feat dir
     for r = 1:nruns
+        
+        % Figure out the name of the run. This is very specific to
+        % Spitschan's naming convention.
+        tmp = strsplit(d{r}, '_')
+        
+        direction = tmp{3};
+        runNum = tmp{2};
+        func_new = [direction '_' runNum];
+        
         % Overwrite example_func2standard.nii.gz
-        system(['flirt -in ' fullfile(session_dir,d{r},feat_dir, func) ...
+        system(['flirt -in ' fullfile(session_dir,d{r}, func) ...
             ' -ref ' fullfile(session_dir,d{r},feat_dir,'reg','standard.nii.gz') ' -out ' ...
-            fullfile(outdir, [func_new, '.standard.nii.gz') ...
+            fullfile(outdir, [func_new, '.standard.nii.gz']) ...
             ' -init ' fullfile(session_dir,d{r},feat_dir,'reg','example_func2standard.mat') ...
             ' -applyxfm']);
     end
