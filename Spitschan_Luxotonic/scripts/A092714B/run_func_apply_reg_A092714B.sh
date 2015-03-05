f6e7e8b495e334ea930bffafb44899aab41a7571
Manuel Spitschan <mspits@sas.upenn.edu>
2015-03-05 13:53:51 -0500

    Register time series to brain

---
+++ Spitschan_Luxotonic/scripts/A092714B/run_func_apply_reg_A092714B.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+matlab -nodisplay -nodesktop -nosplash -r "func_apply_reg({'/data/jag/mspitschan/Imaging/Protocols/MRLuxotonic/A092714B'},'dbrf.tf','/data/jag/mspitschan/Imaging/Protocols/MRLuxotonic/A092x14B'); exit"
---
+++ Spitschan_Luxotonic/scripts/M120514S/run_func_apply_reg_M120514S.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+matlab -nodisplay -nodesktop -nosplash -r "func_apply_reg({'/data/jag/mspitschan/Imaging/Protocols/MRLuxotonic/M120514S'},'dbrf.tf','/data/jag/mspitschan/Imaging/Protocols/MRLuxotonic/M121x14S'); exit"
