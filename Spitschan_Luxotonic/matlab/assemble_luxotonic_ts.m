c27dcc49ebeea0a11a0ae86fd804ad035383e13d
Manuel Spitschan <mspits@sas.upenn.edu>
2015-03-06 15:32:41 -0500

    luxotonic ts avg
    Signed-off-by: Manuel Spitschan <mspits@sas.upenn.edu>

---
+++ Spitschan_Luxotonic/matlab/assemble_luxotonic_ts.m
@@ -0,0 +1,6 @@
+theFig = 1;
+nPlots = 1;
+plotInd = 1;
+roi = 'v1';
+session_dir = '/data/jag/mspitschan/Imaging/Protocols/MRLuxotonic/A092x14B/timeseries'; 
+luxotonic_ts_avg(session_dir, 'Isochromatic', theFig, nPlots, plotInd, roi)
