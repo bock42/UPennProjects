8fb94ce49b12a17ec4cf185b2680249c9a6ae90c
bock42 <abock@mac-pro.uphs.upenn.edu>
2015-05-13 16:47:31 -0400

    CF_MASTER

--- pRF_fitting/CF_MASTER.m
+++ pRF_fitting/CF_MASTER.m
@@ -62,29 +62,10 @@ create_regressors(session_dir);
 temporal_filter(session_dir);
 %% Segment freesurfer aseg.mgz volume
 % Segments the freesurfer anatomical aseg.mgz volume into several ROIs in
-%   the session_dir:
-%
-%   brain.nii.gz
-%   aseg.gm.nii.gz
-%   aseg.wm.nii.gz
-%   aseg.lh_ventricle.nii.gz
-%   aseg.rh_ventricle.nii.gz
-%   aseg.third_ventricle.nii.gz
-%   aseg.fourth_ventricle.nii.gz
-%   aseg.brainstem.nii.gz
-%   aseg.unknown.nii.gz
+%   the session_dir.
 segment_anat(session_dir,subject_name);
 %% Project anatomical ROIs to functional space
-% Projects anatomical ROIs into functional space in each bold directory:
-%   <func>.brain.nii.gz
-%   <func>.aseg.gm.nii.gz
-%   <func>.aseg.wm.nii.gz
-%   <func>.aseg.lh_ventricle.nii.gz
-%   <func>.aseg.rh_ventricle.nii.gz
-%   <func>.aseg.third_ventricle.nii.gz
-%   <func>.aseg.fourth_ventricle.nii.gz
-%   <func>.aseg.brainstem.nii.gz
-%   <func>.aseg.unknown.nii.gz
+% Projects anatomical ROIs into functional space in each bold directory.
 project_anat2func(session_dir,subject_name);
 %% Create localWM timecourses
 % Creates local white matter timecourses for each voxel, and saves these
@@ -109,7 +90,6 @@ xhemi_check(session_dir,subject_name);
 project_template(session_dir,subject_name)
 %% Create occipital ROI
 create_occipital(session_dir,subject_name);
-
 %% Calculate cortical distance (e.g. in V1)
 hemis = {'lh' 'rh'};
 for hh = 1:length(hemis)
@@ -117,101 +97,18 @@ for hh = 1:length(hemis)
     calc_surface_distance(session_dir,subject_name,hemi);
 end
 %% run CF
-run_CF(session_dir,subject_name)
-
+%   Usage:
+%   run_CF(session_dir,subject_name,runNum,hemi,srcROI,trgROI,DoG,seedSig1,seedSig2,seedSig3,seedSig4)
+run_CF(session_dir,subject_name);
 %% Average CF runs
-template = 'prf';
-func = 'sdbrf.tf';
-condition = 'movie';
-runs=[2 4 6]; % for AEK, bars = [1 2 5], movie = [3 4 6];
-roi = 5; % 1=V1; 2=V1_V3; 3=occipital; 4 = cortex; 5=subcortical;
-hemi = {'lh'};
-average_CF_runs(session_dir,condition,runs,func,template,roi,hemi)
+%   Usage:
+%   average_CF(session_dir,subject_name,runs,srcROI,trgROI,hemis)
+average_CF(session_dir,subject_name,runs);
+%% Project to cvs MNI space
+project_CF_cvsMNI(session_dir,subject_name);
+%% Plot CF map
+plot_CF_maps(session_dir);
 
-%%
-session_dir = '/Users/abock/data/Retinotopy/ASB/10272014';
-subject_name = 'ASB_10272014_MPRAGE_ACPC_7T';
-% seedSig1 = linspace(1,5,3); % millimeters
-% seedSig2 = linspace(6,10,3);
-% seedSig3 = linspace(0,10,3);
-% seedSig = [seedSig1',seedSig2',seedSig3'];
-% seedSig1 = (.5:.1:15)';
-% seedSig2 = (1.25:0.25:2)';
-% seedSig3 = (0:0.1:1)';
 
-% Use this for actual analysis
-% seedSig1 = (.5:.25:15)';
-% seedSig2 = (1.25:0.25:3)';
-% seedSig3 = (0:0.1:1)';
-% Use this for testing
-seedSig1 = (1:2:9)';
-seedSig2 = (1:.5:3)';
-seedSig3 = (0:0.25:1)';
-seedSig4 = (0:0.25:1)';
-seedSig = {seedSig1 seedSig2 seedSig3 seedSig4};
 
-hemis = {'lh' 'rh'};
-space = 'volume';
-srcROI = 'volume';
-trgROI = 'prf_V1';
-DoG = 1; % difference of Gaussians
-% Find bold run directories
-d = listdir(fullfile(session_dir,'*BOLD_*'),'dirs');
-if isempty(d)
-    d = listdir(fullfile(session_dir,'*bold_*'),'dirs');
-end
-if isempty(d)
-    d = listdir(fullfile(session_dir,'*EPI_*'),'dirs');
-end
-if isempty(d)
-    d = listdir(fullfile(session_dir,'RUN*'),'dirs');
-end
-nruns = length(d);
-disp(['Session_dir = ' session_dir]);
-disp(['Number of runs = ' num2str(nruns)]);
-for rr = [2 4 6];
-    for hh = 1:length(hemis)
-        hemi = hemis{hh};
-        % Get source indices
-        if strcmp(trgROI,'V1')
-            areas = load_nifti(fullfile(session_dir,[hemi '.areas.nii.gz']));
-        elseif strcmp(trgROI,'prf_V1')
-            areas = load_nifti(fullfile(session_dir,[hemi '.areas_pRF.nii.gz']));
-        end
-        if strcmp(srcROI,'V3');
-            V1ind = find(areas.vol<=1 & areas.vol >=-1);
-            V1_3ind = find(areas.vol<=3 & areas.vol >=-3);
-            V1_3ind(V1ind) = 0;
-            srcind = find(V1_3ind);
-        elseif strcmp(srcROI,'cortex')
-            srcind = 1:length(areas.vol); % entire cortex
-        elseif strcmp(srcROI,'volume')
-            binfile = fullfile(session_dir,d{rr},'single_TR.nii.gz');
-            src = load_nifti(binfile);
-            srcind = find(src.vol > 0 & ~isnan(src.vol));
-        end
-        % Get target indices
-        if strcmp(trgROI,'V1');
-            V1 = load_nifti(fullfile(session_dir,[hemi '.areas.nii.gz']));
-            trgind = find(V1.vol<=1 & V1.vol >=-1);
-        elseif strcmp(trgROI,'prf_V1');
-            V1 = load_nifti(fullfile(session_dir,[hemi '.areas_pRF.nii.gz']));
-            trgind = find(V1.vol<=1 & V1.vol >=-1);
-        end
-        if strcmp(srcROI,'volume')
-            srcfile = fullfile(session_dir,d{rr},'sdbrf.tf.nii.gz');
-        elseif strcmp(srcROI,'cortex')
-            srcfile = fullfile(session_dir,d{rr},['sdbrf.tf_surf.' hemi '.nii.gz']);
-        end
-        trgfile = fullfile(session_dir,d{rr},['sdbrf.tf_surf.' hemi '.nii.gz']);
-        do_CF(session_dir,subject_name,rr,space,srcfile,trgfile,srcind,trgind,srcROI,trgROI,seedSig,hemi,DoG);
-    end
-end
-%% Average CF maps across runs and hemispheres
-template = 'prf';
-func = 'sdbrf.tf';
-condition = 'movie';
-runs=[2 4 6]; % for AEK, bars = [1 2 5], movie = [3 4 6];
-roi = 5; % 1=V1; 2=V1_V3; 3=occipital; 4 = cortex; 5=subcortical;
-hemi = {'lh'};
-average_CF_runs(session_dir,condition,runs,func,template,roi,hemi)
+
